ace8ae975df5d265397cdc1ec0168b3fb15b9001
Daniel Rojas <github@danielrojas.net>
2020-10-30 22:03:23 +0100

    Add new `Endpoint` class

--- src/wireviz-oo/links.py
+++ src/wireviz-oo/links.py
@@ -3,25 +3,24 @@
 
 class Connection:
     # defines the classic connection from connector, via conductor, to connector
-    from_id:   Any
-    from_port: Any
-    via_id:    Any
-    via_port:  Any
-    to_id:     Any
-    to_port:   Any
+    source: Endpoint  # from is a Python keyword
+    via: Endpoint
+    destination:  Endpoint
 
 class MatePin:
     # defines a mechanical mate from one pin to another
     # see WireViz feature/mate+autogenerate branch
-    from_id:   Any
-    from_port: Any
-    to_id:     Any
-    to_port:   Any
+    source: Endpoint
+    destination: Endpoint
     shape: str
 
 class MateComponent:
     # defines a mechanical mate from one connector to another
     # see WireViz feature/mate+autogenerate branch
-    from_id: Any
-    to_name: Any
+    source: Designator
+    destination: Designator
     shape: str
+
+class Endpoint:
+    id: Designator
+    port: int
